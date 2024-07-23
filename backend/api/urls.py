39ebb1972c94d9b5975c8b043e01a1f73536eaa4
Duc Minh Nguyen <104491261+ducoday12345@users.noreply.github.com>
2024-07-23 08:16:57 -0400

    create urlpattern in for note change

---
+++ backend/api/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+'''
+NOTE
+pk: primary key 
+this url file is related to note management
+'''
+urlpattern = [
+    path("notes/", views.NoteListCreate.as_view(), name="note-list"),
+    path("notes/delete/<int:pk>/", views.NoteDelete.as_view(), name="delete-note")
+]
