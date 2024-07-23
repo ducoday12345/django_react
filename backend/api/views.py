100dcaacfbf442c82564c1f3f0ed971abdef3683
Duc Minh Nguyen <104491261+ducoday12345@users.noreply.github.com>
2024-07-23 07:59:48 -0400

    add view for Note creating: NoteListCreate

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,11 +1,25 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework import generics
-from .serializers import UserSerializer
+from .serializers import UserSerializer, NoteSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
-
-# Create your views here.
+from .models import Note
+# Create user account view
 class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
+
+class NoteListCreate(generics.ListCreateApiView):
+    serializer_class = NoteSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user #get user obj does not need to specify since IsAuthenticated
+        return Note.objects.filter(author=user)
+    
+    def perform_create(self,serializer):
+        if serializer.is_valid():
+            serializer.save(author=self.request.user)
+        else:
+            print(serializer.errors)
