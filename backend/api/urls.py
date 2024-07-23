4d0b7845a81ed987540d94ad5aa16a43d36f765f
Duc Minh Nguyen <104491261+ducoday12345@users.noreply.github.com>
2024-07-23 08:41:23 -0400

    fixing typo urlpatterns

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -5,7 +5,7 @@ NOTE
 pk: primary key 
 this url file is related to note management
 '''
-urlpattern = [
+urlpatterns = [
     path("notes/", views.NoteListCreate.as_view(), name="note-list"),
     path("notes/delete/<int:pk>/", views.NoteDelete.as_view(), name="delete-note")
 ]
