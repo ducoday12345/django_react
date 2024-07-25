75576a93e0c8b96b26e710859fb8c876673c853b
Duc Minh Nguyen <104491261+ducoday12345@users.noreply.github.com>
2024-07-25 11:27:52 -0400

    change form

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -1,60 +1,65 @@
-import {useState} from "react"
-import api from "../api"
-import { useNavigate } from "react-router-dom"
-import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants"
-import "../styles/Form.css"
+import { useState } from "react";
+import api from "../api";
+import { useNavigate } from "react-router-dom";
+import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants";
+import "../styles/Form.css";
+import LoadingIndicator from "./LoadingIndicator";
 
 //route: path to submit form
 //method: register or log in
-function Form({route, method}) {
-    const [username, setUsername] = useState("")
-    const [password, setPassword] = useState("")
-    const [loading, setLoading] = useState(false)
-    const navigate = useNavigate()
+function Form({ route, method }) {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
 
-    const name = method === "login" ? "Login" : "Register"
+    const name = method === "login" ? "Login" : "Register";
 
-    const handleSubmit = async(e) => {
+    const handleSubmit = async (e) => {
         setLoading(true);
         e.preventDefault();
-        
-        try { 
-            const res = await api.post(route, {username, password})
-            if (method === "login"){ //if the method is login 
+
+        try {
+            const res = await api.post(route, { username, password });
+            if (method === "login") {
+                //if the method is login
                 localStorage.setItem(ACCESS_TOKEN, res.data.access); // Store the access token in local storage
                 localStorage.setItem(REFRESH_TOKEN, res.data.refresh); // Store the refresh token in local storage
-                navigate("/") // Navigate to the home page
-            }else{ // If the method is not "login" ( register)
-                navigate("/login") // Navigate to the login page
+                navigate("/"); // Navigate to the home page
+            } else {
+                // If the method is not "login" ( register)
+                navigate("/login"); // Navigate to the login page
             }
+        } catch (error) {
+            alert(error);
+        } finally {
+            setLoading(false);
         }
-        catch(error){
-            alert(error)
-        }finally {
-            setLoading(false)
-        }
-    }
+    };
 
-    return <form onSubmit={handleSubmit} className="form-container">
-        <h1>{name}</h1>
-        <input
-            className="form-input"
-            type="text"
-            value = {username}
-            onChange={(e) => setUsername(e.target.value)}
-            placeholder="Username"
-        />
-        <input
-            className="form-input"
-            type="password"
-            value = {password}
-            onChange={(e) => setPassword(e.target.value)}
-            placeholder="Password"
-        />
-        <button className="form-button" type="submit">
-            {name}
-        </button>
-    </form>
+    return (
+        <form onSubmit={handleSubmit} className="form-container">
+            <h1>{name}</h1>
+            <input
+                className="form-input"
+                type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                placeholder="Username"
+            />
+            <input
+                className="form-input"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Password"
+            />
+            {loading && <LoadingIndicator />}
+            <button className="form-button" type="submit">
+                {name}
+            </button>
+        </form>
+    );
 }
 
-export default Form
+export default Form;
