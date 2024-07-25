e81073dfc9c0dbfdaff18e99abd3c29268cb2570
Duc Minh Nguyen <104491261+ducoday12345@users.noreply.github.com>
2024-07-25 09:36:37 -0400

    add deleteNote function

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -14,9 +14,17 @@ function Home() {
         api
             .get("/api/notes/")
             .then((res) => res.data)
-            .then((data) => {setNotes(data); console.log(data)})
+            .then((data) => { setNotes(data); console.log(data) })
             .catch((err) => alert(err));
     };
+
+    const deleteNote = (id) => {
+        api.delete(`/api/notes/delete/${id}/`).then((res) => {
+            if (res.status === 204) alert("Note deleted!")
+            else alert("Failed to delete note")
+        })
+
+    }
 
     return <div>Home</div>;
 }
